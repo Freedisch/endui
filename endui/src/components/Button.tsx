1bcc5d75bfb3570e6a752e58162890c32b206bee
freedisch <freeproduc@gmail.com>
2023-10-29 22:48:04 +0100

    update button
    Signed-off-by: freedisch <freeproduc@gmail.com>

--- endui/src/components/Button.tsx
+++ endui/src/components/Button.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import { VariantProps, cva } from 'class-variance-authority';
 import { cn } from '../utils/utils';
 import { Loader2 } from 'lucide-react';
