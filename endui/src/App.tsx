4facddac65c86ca88f22202e55e85274f7a4c80e
freedisch <freeproduc@gmail.com>
2024-01-18 20:51:04 +0200

    render test
    Signed-off-by: freedisch <freeproduc@gmail.com>

--- endui/src/App.tsx
+++ endui/src/App.tsx
@@ -1,8 +1,42 @@
 import './App.css';
-import { Button } from './components/Buttons/Button';
+import { Button, buttonVariants } from './components/Buttons/Button';
+import Heading from './components/Heading/Heading';
+import { Input } from './components/Input/Input';
 
 function App() {
-  return <Button>Hello world</Button>;
+  return (
+    <div>
+      <div>
+        <Button variant="ghost" size="lg">
+          Hello world
+        </Button>
+      </div>
+
+      <div>
+        <Button variant="default" size="sm">
+          Hello world
+        </Button>
+
+        <button
+          className={buttonVariants({
+            variant: 'ghost',
+            className: 'w-fit',
+            size: 'lg',
+          })}
+        >
+          Test button
+        </button>
+      </div>
+
+      <Heading size="lg" className="three-d text-black dark:text-light-gold">
+        Easily determine
+      </Heading>
+
+      <div>
+        <Input className="w-fit truncate" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
